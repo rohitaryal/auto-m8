f81ffc34ab8a4bc456d1b1459998c4f0dcccbc04
rohitaryal <erucix@gmail.com>
2025-02-14 12:27:55 +0530

    fixed everything

--- autom8.py
+++ autom8.py
@@ -1,5 +1,8 @@
 import argparse
 import sys
+import string
+from colorama import Fore
+from undetected_chromedriver.patcher import random
 from utils import Logger
 import utils.scanner as sc
 import utils.apkstore as store
@@ -9,6 +12,17 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from utils.platinmods import post_to_platinmods
 from utils.uploader import upload_to_fileupload, upload_to_modsfire
+
+def generate_random_url(base_url: str, length: int = 12) -> str:
+    """
+    Generates a random URL with a given base URL and a random path of specified length.
+    
+    :param base_url: The base URL (e.g., "https://www.file-upload.org/")
+    :param length: The length of the random path (default is 12 characters)
+    :return: A complete URL with a random path
+    """
+    random_path = ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    return f"{base_url.rstrip('/')}/{random_path}"
 
 parser = argparse.ArgumentParser(
     prog="autom8",
@@ -25,7 +39,7 @@ args = parser.parse_args()
 source_dir = args.directory
 chrome_profile = args.profile or "./profile"
 timeout = args.timeout or 400
-template = args.template or "./templates/shared_mod.bb"
+template = args.template or "./templates/untested_app.bb"
 
 if not source_dir:
     parser.print_help()
@@ -37,60 +51,55 @@ options.add_argument(f"--profile-directory=Default")
 
 Logger.info("Starting chrome")
 driver = uc.Chrome(options=options, headless=False)
-wait = WebDriverWait(driver=driver, timeout=400, poll_frequency=1)
+wait = WebDriverWait(driver=driver, timeout=timeout, poll_frequency=1)
 
 file_list = sc.scan_apps_in_dir(source_dir)
 
 for file in file_list:
-    link1 = upload_to_modsfire(driver, wait, file)
-    link2 = upload_to_fileupload(driver, wait, file)
+    print(f"\n\n{Fore.GREEN}=============== NEW THREAD ============== {Fore.RESET}")
+    Logger.info(f"File: {file}")
 
+    link1 = "" #Logger.input("Link 1")
+    link2 = generate_random_url("https://file-upload.org/")#Logger.input("Link 2")
+
+#    features = Logger.input("Features [Separated by ,]") or "Premium Unlocked"
+    features = random.choice(["Pro Unlocked", "Premium Unlocked", "Premium Subscribed", "VIP Unlocked", "Subscribed VIP"])
+    features = features.split(", ")
+
+    
     if not link1:
-        link1 = Logger.input("Link 1")
-
+        link1 = upload_to_modsfire(driver, wait, file)
     if not link2:
-        link2 = Logger.input("Link 2")
+        link2 = upload_to_fileupload(driver, wait, file)
 
-    if input("Continue: ").strip() == "n":
-        break
+    file_detail = apkfile.get_detail_from_file(file)
 
-    apk_file_details = apkfile.get_detail_from_file(file)
-    apk_store_details = {}
+    if file_detail is None:
+        continue
 
-    if apk_file_details is None:
-        Logger.error(f"Failed to find store details for {file}")
+    store_detail = store.get(file_detail['PACKAGE_NAME'])
 
-        apk_store_details = store.find(Logger.input("Keyword to search"))
-    else:
-        apk_store_details = store.get(apk_file_details['PACKAGE_NAME'])
-
-    if apk_store_details is None:
-        apk_store_details = {
-            'input': Logger.input("App Name"),
-            'version': Logger.input("App Version"),
-            'icon': Logger.input("App Icon"),
-            'url': Logger.input("Store URL"),
-            'features': ", ".split(Logger.input("Features[Seperated by ', ']")),
-        }
-
-    if not 'features' in apk_store_details:
-        apk_store_details['features'] = 'Premium Unlocked'
+    if store_detail is None:
+        store_detail = store.get(store.find(file_detail['PACKAGE_NAME'])[0]['appId'])
+        if store_detail is None:
+            store_detail = {
+                'title': '',
+                'version': '',
+                'url': '',
+                'icon': '',
+            }
 
     post_to_platinmods(
-        driver=driver,
-        wait=wait,
-        app_name = apk_store_details['title'],
-        app_version = apk_store_details['version'],
-        app_icon=apk_store_details['icon'],
-        app_link=apk_store_details['url'],
-        app_features=apk_store_details['features'],
-        link_1=link1,
-        link_2=link2,
-        template_path=template,
-    )
-
-    if Logger.input("Continue") == 'n':
-        break
-
+        driver,
+        wait,
+        link_1 = link1 or "",
+        link_2 = link2 or "",
+        app_name = store_detail['title'] or Logger.input("App name"),
+        app_version = store_detail['version'] or Logger.input("App version"),
+        app_link = store_detail['url'] or Logger.input("App link"),
+        app_icon = store_detail['icon'] or Logger.input("App icon"),
+        app_features =  features,
+        template_path = template
+   )
 
 driver.quit()
